9d809ee1143add74a924e111be385f2d992adcad
aFages <sio.fages@gmail.com>
2016-11-14 14:16:29 +0100

    test3

---
+++ include/entete.php
@@ -0,0 +1,15 @@
+<div id="container">
+	<div id="header">
+    	<h1><a href="/">sworm</a></h1>
+        <h2>your website slogan here</h2>
+        <div class="clear"></div>
+    </div>
+    <div id="nav">
+    	<ul>
+        	<li class="start selected"><a href="index.html">Home</a></li>
+            <li><a href="examples.html">Examples</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Solutions</a></li>
+            <li class="end"><a href="#">Contact</a></li>
+        </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>sworm - Free CSS Template by ZyPOP</title>
+<link rel="stylesheet" href="css/styles.css" type="text/css" />
+<!--
+sworm, a free CSS web template by ZyPOP (zypopwebtemplates.com/)
+
+Download: http://zypopwebtemplates.com/
+
+License: Creative Commons Attribution
+//-->
+</head>
+<body>
+    <?php 
+        include("include/entete.php");
+    ?>  
+
+    <div id="body">
+		<div id="content">
+			<h2>Introduction</h2>
+            <p>Welcome to sworm, a free premium valid CSS &amp; XHTML strict web template from <a href="http://zypopwebtemplates.com/" title="ZyPOP">ZyPOP</a>. This template is completely <strong>free</strong> to use permitting a link remains back to  <a href="http://zypopwebtemplates.com/" title="ZyPOP">http://zypopwebtemplates.com/</a>. Should you wish to use this template unbranded you can buy a template license from our website for 8.00 GBP, this will allow you remove all branding related to our site, for more information about this see below.</p>	
+            
+            <p>This template has been tested in:</p>
+
+
+            <ul class="styledlist">
+                <li>Firefox</li>
+                <li>Opera</li>
+                <li>IE 6, 7, 8 and 9</li>
+                <li>Chrome</li>
+            </ul>
+    
+            <h3>Buy unbranded</h3>
+
+
+
+            <p>Purchasing a template license for 8.00 GBP (at time of writing around 12 USD) gives you the right to remove any branding including links, logos and source tags relating to ZyPOP. As well as waiving the attribution requirement, your payment will also help us provide continued support for users as well as creating new web templates. Find out more about how to buy at the licensing page on our website which can be accessed at <a href="http://zypopwebtemplates.com/licensing" title="template license">http://zypopwebtemplates.com/licensing</a></p>
+
+<h3>Lorem lipsum</h3>
+
+<p>Morbi fermentum condimentum felis, commodo vestibulum sem mattis sed. Aliquam magna ante, mollis vitae tincidunt in, malesuada vitae turpis. Sed aliquam libero ut velit bibendum consectetur. Quisque sagittis, est in laoreet semper, enim dui consequat felis, faucibus ornare urna velit nec leo. Maecenas condimentum velit vitae est lobortis fermentum. In tristique sem vitae metus ornare luctus tempus nisl volutpat. Integer et est id nisi tempus pharetra sagittis et libero.</p>
+
+
+        </div>
+        
+        <div class="sidebar">
+            <ul>	
+               <li>
+                    <h3>Navigate</h3>
+                    <ul class="blocklist">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="examples.html">Examples</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Solutions</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </li>
+                
+                <li>
+                    <h3>About</h3>
+                    <ul>
+                        <li>
+                        	<p style="margin: 0;">Aenean nec massa a tortor auctor sodales sed a dolor. Duis vitae lorem sem. Proin at velit vel arcu pretium luctus.</p>
+                        </li>
+                    </ul>
+                </li>
+                
+                <li>
+                	<h3>Search</h3>
+                    <ul>
+                    	<li>
+                            <form method="get" class="searchform" action="http://wpdemo.justfreetemplates.com/" >
+                                <p>
+                                    <input type="text" size="12" value="" name="s" class="s" />
+                                    <input type="submit" class="searchsubmit formbutton" value="Search" />
+                                </p>
+                            </form>	
+						</li>
+					</ul>
+                </li>
+                
+                <li>
+                    <h3>Sponsors</h3>
+                    <ul>
+                        <li><a href="http://www.themeforest.net/?ref=spykawg" title="premium templates"><strong>ThemeForest</strong></a> - premium HTML templates, WordPress themes and PHP scripts</li>
+                        <li><a href="http://www.dreamhost.com/r.cgi?259541" title="web hosting"><strong>Web hosting</strong></a> - 50 dollars off when you use promocode <strong>awesome50</strong></li>
+                        <li><a href="http://www.4templates.com/?aff=spykawg" title="4templates"><strong>4templates</strong></a> - brilliant premium templates</li>
+                    </ul>
+                </li>
+                
+            </ul> 
+        </div>
+    	<div class="clear"></div>
+    </div>
+
+    <?php 
+        include("include/pieddepages.php");
+    ?>  
+
+</div>
+</body>
+</html>
